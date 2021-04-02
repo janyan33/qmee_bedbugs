3b7deab3a489ecbbcb7c9d2af33367bf2c85bc50
Janice Yan <janyan33@gmail.com>
2021-04-02 19:14:36 -0400

    Cleaned functions script

--- scripts/functions.R
+++ scripts/functions.R
@@ -14,32 +14,6 @@ func_igraph <- function(rep_groups){
   return(igraph)
 }
 
-## FUNCTION 2: Runs permutations for effect of strength 
-func_permute_strength <- function(igraph_object){
-  ## Calculating observed coef for effect of strength
-  observed_strength <- as.data.frame(cbind(strength = strength(igraph_object, v = V(igraph_object), mode = c("all"), loops = FALSE), 
-                                           sex = V(igraph_object)$sex))
-  obs_model <- lm(data = observed_strength, strength ~ sex)
-  obs_coef <- coef(obs_model)[2]
-  ## Setting up for permutations
-  nsim <- 999
-  sim_coefs <- numeric(nsim)
-  ## Shuffle network 999 times
-  for(i in 1:nsim){
-  sim_igraph <- permute(igraph_object, sample(vcount(igraph_object)))
-  new_strength <- as.data.frame(cbind(strength = strength(sim_igraph, v = V(sim_igraph), mode = c("all"), loops = FALSE), 
-                                                           sex = V(sim_igraph)$sex))
-    
-  new_model <- lm(data = new_strength, strength ~ sex)
-  sim_coef <- coef(new_model)[2]
-  
-  sim_coefs[i] <- sim_coef
-  }
-  strength_results <- c(obs_coef, sim_coefs)
-  strength_results
-}
-
-
 ## FUNCTION 2: Assigns additional attributes (size, replicate, treatment) to each node on each igraph object
 # Input: Takes a lists of igraph objects
 # Ourput: Creates a dataframe of node attributes
@@ -72,9 +46,8 @@ func_permute_igraph <- function(rep_list_group) {
   gbi_matrix <- get_group_by_individual(group_list, data_format = "groups")
   ibi_matrix <- get_network(gbi_matrix, data_format = "GBI")
   
-  
- new_names <- sample(colnames(ibi_matrix))
-colnames(ibi_matrix) <- new_names
+  new_names <- sample(colnames(ibi_matrix))
+  colnames(ibi_matrix) <- new_names
   rownames(ibi_matrix) <- new_names
  
   igraph <- graph_from_adjacency_matrix(ibi_matrix, diag = FALSE, weighted = TRUE, mode = "undirected")
