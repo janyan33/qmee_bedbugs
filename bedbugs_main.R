a39e9b51928572f73f6fb9d5b54e4de76cca07b5
TovahKashetsky <faith@Tovahs-Mac.local>
2021-04-05 17:13:45 -0400

    Updating

--- bedbugs_main.R
+++ bedbugs_main.R
@@ -21,7 +21,7 @@ attr <- read.csv("data/bbsna_attributes.csv") %>%
         filter(replicate == 1 | replicate == 2) %>% 
         filter(notes != "died")
 
-################# OBSERVED AGGREGATION NETWORKS ####################
+#################### OBSERVED AGGREGATION NETWORKS ####################
 # Using func_igraph on rep_list_groups to create a list of igraph objects (1 per replicate)
 igraph_objects <- lapply(rep_list_groups, func_igraph)
 
@@ -32,20 +32,20 @@ print(attr_observed)
 ## Visualizing the observed networks
 lapply(X = igraph_objects, FUN = func_plot_network)
 
-################# PREDICTION 1 GLM ##########################
-predict1.1 <- glm(prox_strength~sex + thorax.mm, data=attr, family = Gamma(link="log"))
+######################## PREDICTION 1 GLM ##########################
+predict1.1 <- glm(strength~sex + size, data=attr_observed, family = Gamma(link="log"))
 plot(predict1.1) 
 # residuals vs fitted and scale-location not straight
-predict1.2 <- glm(prox_strength~sex + thorax.mm + replicate, data=attr, family = Gamma(link="log"))
+predict1.2 <- glm(strength~sex + size + replicate, data=attr_observed, family = Gamma(link="log"))
 plot(predict1.2) 
 # residuals vs fitted and scale-location look better but still not straight
-thorax.q <- attr$thorax.mm^2
-predict1.3 <- glm(prox_strength~sex + thorax.mm + thorax.q + replicate, data=attr, family = Gamma(link="log"))
+size.q <- attr_observed$size^2
+predict1.3 <- glm(strength~sex + size + size.q + replicate, data=attr_observed, family = Gamma(link="log"))
 plot(predict1.3) 
 obs_strength_coef <- coef(predict1.3)[2]
 #quadratic plots look good # use this?
 
-################# PREDICTION 1 PERMUTATION ##########################
+###################### PREDICTION 1 PERMUTATION ##########################
 n_sim_1 <- 999
 set.seed(33)
 sim_coefs <- numeric(n_sim_1)
@@ -63,27 +63,27 @@ if (obs_strength_coef >= mean(sim_coefs)) {
   }
 text(x = 0.4, y = 100, "p = 0.32")
 
-################ VISUALIZING MALE VS. FEMALE STRENGTH ##############
+################### VISUALIZING MALE VS. FEMALE STRENGTH #################
 ggplot(data = attr_observed, aes(y = strength, x = treatment, fill = sex)) + geom_boxplot() 
 
 ################# PREDICTION 2: ASSORTATIVITY OF INDIVIDUAL NETWORKS #####################
 ibi_matrices <- lapply(X = rep_list_groups, FUN = func_ibi)
 lapply(X = ibi_matrices, FUN = func_permute_assort)
 
-################# PREDICTION 3 GLM ##########################
-social.low <- attr %>% group_by(treatment) %>% filter(treatment=="low") #low treatment data
-social.high <- attr %>% group_by(treatment) %>% filter(treatment=="high") #high treatment data
+##################### PREDICTION 3 GLM ##########################
+social.low <- attr_observed %>% group_by(treatment) %>% filter(treatment=="low") #low treatment data
+social.high <- attr_observed %>% group_by(treatment) %>% filter(treatment=="high") #high treatment data
 
-predict3.low <- glm(matings~prox_strength + thorax.mm, data=social.low, family = Gamma(link="log"))
+predict3.low <- glm(matings~strength + size, data=social.low, family = Gamma(link="log"))
 plot(predict3.low) 
 # all good except residual vs fitted
 # log or quadratic prox_strength made it worse
 
-predict3.high <- glm(matings~prox_strength + thorax.mm, data=social.high, family = Gamma(link="log"))
+predict3.high <- glm(matings~strength + size, data=social.high, family = Gamma(link="log"))
 plot(predict3.high) 
 # plots are not good fit, try log
-social.high.log <- log(social.high$prox_strength)
-predict3.high2 <- glm(matings~prox_strength + social.high.log + thorax.mm, data=social.high, family = Gamma(link="log"))
+social.high.log <- log(social.high$strength)
+predict3.high2 <- glm(matings~strength + social.high.log + thorax.mm, data=social.high, family = Gamma(link="log"))
 plot(predict3.high2 ) 
 # need everything log or just prox_strength?
 
